e685230b4225d24815138fe5e0e4e3af465a6f5a
yuzhen1024 <_@_>
2025-10-19 17:45:05 +0800

    fix: floating window move, and normal window center

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/yuzhen1024/niri-center-onlyone
 
 go 1.24.5
 
-require github.com/buger/jsonparser v1.1.1 // indirect
+require github.com/tidwall/gjson v1.18.0
+
+require (
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
+)
--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"os/exec"
 	"time"
 
-	"github.com/buger/jsonparser"
+	"github.com/tidwall/gjson"
 )
 
 var debounce = NewDebounce(time.Millisecond * 50)
@@ -18,14 +18,18 @@ func main() {
 
 	scanner := bufio.NewScanner(stdout)
 	for scanner.Scan() {
-		_, _, _, err := jsonparser.Get(scanner.Bytes(), "WindowOpenedOrChanged", "window", "id")
-		if err == nil {
+		result := gjson.GetBytes(scanner.Bytes(), "WindowOpenedOrChanged.window.id")
+		if result.Index != 0 {
 			tryCenterWindow()
 		}
-		_, _, _, err = jsonparser.Get(scanner.Bytes(), "WindowClosed")
-		if err == nil {
+		result = gjson.GetBytes(scanner.Bytes(), "WindowClosed")
+		if result.Index != 0 {
 			tryCenterWindow()
 		}
+		// _, _, _, err = jsonparser.Get(scanner.Bytes(), "WindowLayouts")
+		// if err == nil {
+		// 	tryCenterWindow()
+		// }
 	}
 }
 
@@ -35,23 +39,28 @@ func tryCenterWindow() {
 	} else {
 		debounce.TryLock()
 	}
-	// count += 1
+	count += 1
 	// fmt.Println("try...", count)
 
 	focusedWindowData, _ := exec.Command("niri", "msg", "--json", "focused-window").CombinedOutput()
-	focusedWindowworkspaceID, _ := jsonparser.GetInt(focusedWindowData, "workspace_id")
-	count := 0
+	if gjson.GetBytes(focusedWindowData, "is_floating").Bool() {
+		return
+	}
+	focusedWindowworkspaceID := gjson.GetBytes(focusedWindowData, "workspace_id").Int()
 
+	count := 0
 	windowsData, _ := exec.Command("niri", "msg", "--json", "windows").CombinedOutput()
-	jsonparser.ArrayEach(windowsData, func(value []byte, dataType jsonparser.ValueType, offset int, err error) {
-		workspaceID, _ := jsonparser.GetInt(value, "workspace_id")
+	gjson.ParseBytes(windowsData).ForEach(func(key, value gjson.Result) bool {
+		workspaceID := gjson.Get(value.Raw, "workspace_id").Int()
 		if workspaceID == focusedWindowworkspaceID {
 			count += 1
 		}
-		// if count > 1 {}
+		if count > 1 {
+			return false
+		}
+		return true
 	})
 	if count == 1 {
-		exec.Command("niri", "msg", "action", "center-column").Start()
+		go exec.Command("niri", "msg", "action", "center-column").Run()
 	}
-
 }
